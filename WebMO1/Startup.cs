98065bed6ce9f7dee75e48bc4d3b93108516524f
MOFLAHER <MOFLAHER@MICHAELPC>
2017-01-05 05:49:48 -0800

    Add project files.

---
+++ WebMO1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebMO1.Startup))]
+namespace WebMO1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
